0e9a2d3927aa41f4ef1760ad0c31ed0e8d69a622
MA <36831218+rmaomina@users.noreply.github.com>
2023-02-20 11:25:03 +0900

    Time: 105 ms (19.8%), Space: 54.2 MB (7.14%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.js
+++ 0053-maximum-subarray/0053-maximum-subarray.js
@@ -3,14 +3,16 @@
  * @return {number}
  */
 
+// solved DP 1
 var maxSubArray = function(nums) {
-  let currSum = nums[0];
-  let maxSum = nums[0];
+  if (nums=== null || nums.length === 0) return 0
 
-  for (let i = 1; i < nums.length; i++) {
-    currSum = Math.max(currSum + nums[i], nums[i]);
-    maxSum = Math.max(currSum, maxSum)
-    console.log(currSum, maxSum)
+  let dp = new Array(nums.length).fill(0);
+  dp[0] = nums[0]
+
+  for(let i = 1; i < nums.length; i++) {
+    dp[i] = Math.max(dp[i - 1] + nums[i], nums[i]);
   }
-  return maxSum;
-};
+  console.log(dp)
+  return Math.max(...dp);
+}
