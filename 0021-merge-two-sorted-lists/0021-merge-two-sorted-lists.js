7a918ae81a69def02cac7eff3715346f5e85b287
MA <36831218+rmaomina@users.noreply.github.com>
2023-03-10 17:41:40 +0900

    Time: 170 ms (5%), Space: 51.1 MB (5.9%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.js
@@ -13,6 +13,8 @@
 
 var mergeTwoLists = function(list1, list2) {
   if (!list1 || !list2) return list1 || list2
+  console.log(list1[0])
+  console.log(list1)
 
   if (list1.val < list2.val) {
     list1.next = mergeTwoLists(list1.next, list2)
