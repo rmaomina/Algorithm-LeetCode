2b0b97da66e48f404fcbec4ec477cca8bc63688a
MA <36831218+rmaomina@users.noreply.github.com>
2023-03-15 17:12:10 +0900

    Time: 76 ms (70.97%), Space: 43.8 MB (92.84%) - LeetHub

--- 0994-rotting-oranges/0994-rotting-oranges.js
+++ 0994-rotting-oranges/0994-rotting-oranges.js
@@ -5,11 +5,12 @@
 var orangesRotting = function(grid) {
   const rows = grid.length;
   const cols = grid[0].length;
-  const directions = [[0, 1], [0, -1], [1, 0], [-1, 0]]; // 4-directions
+  // 상,하,좌,우 배열 
+  const directions = [[-1, 0], [1, 0], [0, -1], [0, 1]]; 
   let queue = [];
-  let minutesElapsed = 0;
+  let minutes = 0;
 
-  // Find all rotten oranges
+  // 상한 오렌지를 큐에 담는다. 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       if (grid[i][j] === 2) {
@@ -18,15 +19,16 @@ var orangesRotting = function(grid) {
     }
   }
 
-  // BFS to rot oranges
+  // BFS 상한 오렌지 주변의 셀을 탐색해
+  // 신선한 오렌지가 있으면 썩히고 큐에 넣고 체크(infected)한다. 
   while (queue.length > 0) {
     let size = queue.length;
     let infected = false;
     for (let i = 0; i < size; i++) {
-      const [curI, curJ] = queue.shift();
+      const [currI, currJ] = queue.shift();
       for (let direction of directions) {
-        const nextI = curI + direction[0];
-        const nextJ = curJ + direction[1];
+        const nextI = currI + direction[0];
+        const nextJ = currJ + direction[1];
         if (nextI >= 0 && nextI < rows && nextJ >= 0 && nextJ < cols && grid[nextI][nextJ] === 1) {
           grid[nextI][nextJ] = 2;
           queue.push([nextI, nextJ]);
@@ -34,12 +36,14 @@ var orangesRotting = function(grid) {
         }
       }
     }
+
+    // 썩힌 오렌지가 있으면 minute를 +1한다. 
     if (infected) {
-      minutesElapsed++;
+      minutes++;
     }
   }
 
-  // Check if there are still fresh oranges left
+  // 탐색 후에 신선한 오렌지가 남아있으면 -1을 반환한다. 
   for (let i = 0; i < rows; i++) {
     for (let j = 0; j < cols; j++) {
       if (grid[i][j] === 1) {
@@ -48,5 +52,5 @@ var orangesRotting = function(grid) {
     }
   }
 
-  return minutesElapsed;
+  return minutes;
 };
